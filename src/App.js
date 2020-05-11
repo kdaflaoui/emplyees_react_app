df3ff7f41a6781a03c84f5493569db291dc5b031
MyPC <daflaoui.karim@yahoo.com>
2020-05-11 13:48:07 -0400

    add a nav menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Department from './components/Department';
+import Employee from './components/Employee';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Menu from './components/Menu';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+        <div className="container">
+          <Menu />
+
+          <h3 className="m-3 d-flex justify-content-center">React JS with web api as source of data</h3>
+          <h5 className="m-3 d-flex justify-content-center">Employee Management Portal</h5>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/department" component={Department} />
+            <Route path="/employee" component={Employee} />
+          </Switch>
+
+        {/*
+                <Home />
+        <Department />
+        <Employee />
+        */}
+
     </div>
+    </BrowserRouter>
+
   );
 }
 
